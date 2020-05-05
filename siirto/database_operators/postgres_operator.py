c31b6e5a6d33c4ede16d98404e76723e8059624d
Lokesh Lal <lokeshlal@gmail.com>
2020-05-05 08:17:57 +0530

    modified pg default CDC plugin

--- siirto/database_operators/postgres_operator.py
+++ siirto/database_operators/postgres_operator.py
@@ -1,5 +1,6 @@
 from siirto.database_operators.base_database_operator import BaseDataBaseOperator
 from siirto.shared.enums import DatabaseOperatorType
+import psycopg2
 
 
 class PostgresOperator(BaseDataBaseOperator):
@@ -12,6 +13,7 @@ class PostgresOperator(BaseDataBaseOperator):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.connection = psycopg2.connect(self.connection_string)
 
     def execute(self):
         # lopad plugins
